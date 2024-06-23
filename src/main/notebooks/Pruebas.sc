97af8a462b4dd9f6f8ee7427eefef5017853ee3f
fernandochicote <fechicot@gmail.com>
2024-06-23 11:59:20 +0200

    Primera funcion en Scala

---
+++ src/main/notebooks/Pruebas.sc
@@ -0,0 +1,15 @@
+def contarPalabras(texto: String): Map[String, Int] = {
+  texto.split("\\s+")
+    .map(_.toLowerCase)
+    .groupBy(identity)
+    .view.mapValues(_.length)
+    .toMap
+}
+
+// Ejemplo de ejecución
+val textoEjemplo = "Este es un ejemplo de texto. Este texto contiene palabras repetidas."
+val resultadoConteo = contarPalabras(textoEjemplo)
+println("Conteo de palabras:")
+resultadoConteo.foreach { case (palabra, conteo) =>
+  println(s"$palabra: $conteo")
+}
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,4 +2,20 @@ object Main {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
   }
+
+  def contarPalabras(texto: String): Map[String, Int] = {
+    texto.split("\\s+")
+      .map(_.toLowerCase)
+      .groupBy(identity)
+      .view.mapValues(_.length)
+      .toMap
+  }
+
+  // Ejemplo de ejecución
+  val textoEjemplo = "Este es un ejemplo de texto. Este texto contiene palabras repetidas."
+  val resultadoConteo = contarPalabras(textoEjemplo)
+  println("Conteo de palabras:")
+  resultadoConteo.foreach { case (palabra, conteo) =>
+    println(s"$palabra: $conteo")
+  }
 }
